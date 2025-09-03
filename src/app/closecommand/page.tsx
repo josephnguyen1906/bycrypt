10a2199232e24e683d4b843ea7dacaa0ea861529
Joseph Huy <josephnguyen1906@gmail.com>
2025-09-03 14:51:36 +0700

    update code

--- src/app/closecommand/page.tsx
+++
@@ -1,7 +0,0 @@
-import AssetPage from "@/pages/Asset/Asset.page";
-import HistoryPage from "@/pages/History/History.page";
-import React from "react";
-
-export default function page() {
-  return <HistoryPage value={1} />;
-}
--- src/app/mystaking/page.tsx
+++
@@ -1,7 +0,0 @@
-import HistoryPage from "@/pages/History/History.page";
-import StakingPage from "@/pages/Staking/Staking.page";
-import React from "react";
-
-export default function page() {
-  return <HistoryPage value={4} />;
-}
