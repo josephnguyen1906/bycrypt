6fefb336279cd8dd71f34d7c4c5939578dd32dd3
Joseph Nguyen <josephnguyen1906@gmail.com>
2026-05-13 16:04:37 +0700

    fix error when build

--- src/app/convert/page.tsx
+++ src/app/convert/page.tsx
@@ -2,5 +2,5 @@ import DepositWithdrawPage from "@/pages/DepositWithdraw/DepositWithdraw.page";
 import React from "react";
 
 export default function page() {
-  return <DepositWithdrawPage value={1} />;
+  return <DepositWithdrawPage />;
 }
